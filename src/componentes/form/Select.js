10299686511ab29b394a964ce2168a012c74eae0
innsanowr <luisdev9225@gmail.com>
2022-08-25 00:26:49 -0300

    Criação do backend

--- src/componentes/form/Select.js
+++ src/componentes/form/Select.js
@@ -1,12 +1,17 @@
 
 import styles from './Select.module.css'
 
-const Select = ( {text, name, options, handleOnChange,value} ) => {
+const Select = ({ text, name, options, handleOnChange, value }) => {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}</label>
             <select name={name} id={name}>
                 <option value="">Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={options.id} key={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     )
--- src/componentes/project/ProjectForm.js
+++ src/componentes/project/ProjectForm.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-const ProjectForm = ({btnText}) => {
+const ProjectForm = ({ btnText }) => {
+
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input
@@ -23,9 +44,10 @@ const ProjectForm = ({btnText}) => {
             <Select
                 name='category_id'
                 text='Selecione a categoria'
+                options={categories}
             />
 
-            <SubmitButton text={btnText}/>
+            <SubmitButton text={btnText} />
         </form>
     )
 }
